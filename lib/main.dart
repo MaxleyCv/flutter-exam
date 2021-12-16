93972345ca564a0b074f340686338e0e70f38ebc
maxleszcz <maksym@neuroflow.com>
2021-12-16 19:15:55 +0200

    EXAM: added code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Exam',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Exam'),
     );
   }
 }
@@ -32,84 +26,182 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Widget TextBox(Color color, String text) {
+  return Container(
+    width: 100,
+    height: 40,
+    child: Text(
+      text,
+      style: TextStyle(color: color),
+    ),
+  );
+}
 
-  void _incrementCounter() {
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> _firstColumn = [
+    "Left 1",
+    "Left 2",
+    "Left 3",
+    "Left 4",
+    "Left 5"
+  ];
+  final List<String> _secondColumn = [
+    "Right 1",
+    "Right 2",
+    "Right 3",
+    "Right 4",
+    "Right 5"
+  ];
+
+  void _shuffleBlocks() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      Random rand = new Random();
+
+      int c1Length = _firstColumn.length;
+      int c2Length = _secondColumn.length;
+
+      int columnToChoose = rand.nextInt(2);
+      if (c2Length == 0 || (columnToChoose == 0 && c1Length != 0)) {
+        String elementToChange = _firstColumn[rand.nextInt(c1Length)];
+        _firstColumn.remove(elementToChange);
+        _secondColumn.add(elementToChange);
+      } else {
+        String elementToChange = _secondColumn[rand.nextInt(c2Length)];
+        _secondColumn.remove(elementToChange);
+        _firstColumn.add(elementToChange);
+      }
+    });
+  }
+
+  void _takeBlockHome() {
+    setState(() {
+      List<String> redsInLeft = [];
+      List<String> redsInRight = [];
+      int counter = 0;
+      for (String text in _firstColumn) {
+        if (text.contains("Right")) {
+          redsInLeft.add(text);
+          counter += 1;
+        }
+      }
+
+      for (String text in _secondColumn) {
+        if (text.contains("Left")) {
+          redsInRight.add(text);
+          counter += 1;
+        }
+      }
+
+      int leftLength = redsInLeft.length;
+      Random rand = new Random();
+
+      int el = rand.nextInt(counter);
+
+      if (el < leftLength){
+        _firstColumn.remove(redsInLeft[el]);
+        _secondColumn.add(redsInLeft[el]);
+      }
+      else {
+        el -= leftLength;
+        _secondColumn.remove(redsInRight[el]);
+        _firstColumn.add(redsInRight[el]);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    bool discrepancyFound = false;
+
+    List<Widget> leftColumn = [];
+    for (String text in _firstColumn) {
+      leftColumn
+          .add(TextBox(text.contains("Left") ? Colors.grey : Colors.red, text));
+      if (text.contains("Right")) {
+        discrepancyFound = true;
+      }
+    }
+
+    List<Widget> rightColumn = [];
+    for (String text in _secondColumn) {
+      rightColumn.add(
+          TextBox(text.contains("Right") ? Colors.grey : Colors.red, text));
+      if (text.contains("Left")) {
+        discrepancyFound = true;
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: leftColumn,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: rightColumn,
+                  ),
+                )
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Stack(
+        fit: StackFit.expand,
+        children: [
+          discrepancyFound
+              ? Positioned(
+                  left: 30,
+                  bottom: 20,
+                  child: FloatingActionButton(
+                    heroTag: 'back',
+                    onPressed: _takeBlockHome,
+                    child: const Icon(
+                      Icons.home,
+                      size: 40,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              : Container(),
+          Positioned(
+            bottom: 20,
+            right: 30,
+            child: FloatingActionButton(
+              heroTag: 'next',
+              onPressed: _shuffleBlocks,
+              child: const Icon(
+                Icons.ramen_dining,
+                size: 40,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
